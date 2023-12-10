565703c570c0c5c63f510ef69c19b3cfa803744d
Dasturchi-Mufid <shahbozbekahmadaliyev@gmail.com>
2023-12-10 20:16:47 +0500

    5-darsdagi xatolar togilandi. homework.py qoshildi

---
+++ 5-oy/6-dars/homework.py
@@ -0,0 +1,103 @@
+# 1) Hafta kunlari hisoblaydigan. class yozing. o`sha classning obyekti iteratsiyaga tushganda hafta kunlarining elementlarini qaytarsin 
+# 2) Oylar hisoblaydigan class yozing. o`sha classni obyektini iteratsiaya solganda oylar ketma-ketligini qaytarsin 
+# 3) User degan class yarating, 3ta xususiyati bo`lsin. 1) f_name 2) l_name, 3) email.
+# User obyektini iteratsiyaga solganda shu class asosida yaratilingan obyektlarni qaytarsin.
+
+# Task 1
+
+class Week:
+    
+    """
+    Obyekt yaratilayotganda unga 2 tagacha element berish mumkin.
+    Bitta element berilsa, masalan 4 unda 4 ta hafta kuni chiqadi.
+    Ikkita element berilsa masalan 2, 6 unda 2-hafta kunidan 6-kungacha chiqadi
+    Uch va undan ortiq element berilsa, Type error xatoligi chiqadi 
+    """
+    
+    week_days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+
+    def __init__(self,*args):
+        self.index = 0
+        match len(args):
+            case 0:
+                self.days = Week.week_days
+            case 1:
+                self.days = Week.week_days[:args[0]]
+            case 2:
+                self.days = Week.week_days[args[0]:args[1]]
+            case _:
+                raise TypeError('Week.__doc__ orqali bu classdan qanday qilib ishlash mumkinligi haqida tanishib chiqing.')
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.index <= len(self.days)-1:
+            res = self.days[self.index]
+            self.index += 1
+            return res
+        else:
+            raise StopIteration
+
+
+class Month:
+
+    """
+    Obyekt yaratilayotganda unga 2 tagacha element berish mumkin.
+    Bitta element berilsa, masalan 4, unda 4 ta oy chiqadi.
+    Ikkita element berilsa masalan 2, 6 unda 2-oydan 6-oygacha chiqadi
+    Uch va undan ortiq element berilsa, Type error xatoligi chiqadi 
+    """
+    
+    months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+
+    def __init__(self,*args):
+        self.index = 0
+        match len(args):
+            case 0:
+                self.month = Month.months
+            case 1:
+                self.month = Month.months[:args[0]]
+            case 2:
+                self.month = Month.months[args[0]:args[1]]
+            case _:
+                raise TypeError('Month.__doc__ orqali bu classdan qanday qilib ishlash mumkinligi haqida tanishib chiqing.')
+
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.index <= 11:
+            res = self.month[self.index]
+            self.index += 1
+            return res
+        else:
+            raise StopIteration
+
+class User:
+
+    users = []
+    
+    def __init__(self,fname,lname,email):
+        self.fname = fname
+        self.lname = lname
+        self.email = email
+        self.index = 0
+        User.users.append(self)
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.index < len(User.users):
+            res = User.users[self.index]
+            self.index += 1
+            return res
+        else:
+            raise StopIteration
+        
+    def __str__(self):
+        return f'{self.fname} {self.lname}'
+
+
--- 5-oy/6-dars/main.py
+++ 5-oy/6-dars/main.py
@@ -4,9 +4,9 @@
 # iter() funksiyasi iteratsiya hosil qilib beradi. next() funksiyasi iteratsiyaga tushugan elementni bita bita chaqirib beradi
 # iter va next funksiyalari for siklida foydalaniladi. iter,next,StopIteration shu 3 lasiga asoslangan holda ishlaydi for sikli.
 
-# numbers = [16,30,7,12,'salom']
+numbers = [16,30,7,12,'salom']
 
-# iterable = iter(numbers)
+iterable = iter(numbers)
 
 # print(next(iterable))
 # print(next(iterable))
@@ -25,6 +25,74 @@ def for_func(lst:list):
 
 color = ['oq','qora','yashil','sariq']
 
-for_func(color)
+# for_func(color)
+
+
+class MyIter:
+
+    def __init__(self,*args):
+        self.elements = args
+        self.index = 0
+    
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if len(self.elements)>self.index:
+            result = self.elements[self.index]
+            self.index += 1
+            return result
+        else:
+            raise StopIteration
+
+
+class Myrange:
+
+    def __init__(self,*args):
+        
+        num_args = len(args)
+
+        # match len(args):
+        #     case 0:
+        #         raise TypeError("MyRange expected at least 1 argument, got 0")
+        #     case 1:
+        #         self.start, self.stop, self.step = 0, args[0], 1
+        #     case 2:
+        #         self.start, self.stop, self.step = args[0], args[1], 1
+        #     case 3:
+        #         self.start, self.stop, self.step = args
+        #     case _:
+        #         raise TypeError(f"MyRangega ko`pi bilan 3 ta property berish kerak. Siz {num_args} ta property berdingiz")
+
+        if num_args == 0:
+            raise TypeError("MyRangega kamida 1 ta property berish kerak. Siz umuman property bermadingiz")
+        elif num_args == 1:
+            self.start, self.stop, self.step = 0, args[0], 1
+        elif num_args == 2:
+            self.start, self.stop, self.step = args[0], args[1], 1
+        elif num_args == 3:
+            self.start, self.stop, self.step = args
+        else:
+            raise TypeError(f"MyRangega ko`pi bilan 3 ta property berish kerak. Siz {num_args} ta property berdingiz")
+        
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.stop > self.start:
+            result = self.strat
+            self.strat += self.step
+            return result
+        else:
+            raise StopIteration
+
+a = MyIter(1,250,15)
+for i in a:
+    print(i)
+# for i in Myrange(10):
+#     print(i)
+        
+
+
 
 
